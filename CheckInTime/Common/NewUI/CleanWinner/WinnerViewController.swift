6e9c463aed7d2738daa3e8e1ef896e2effaed4b3
DatTran <dattran240496@gmail.com>
2018-01-24 16:21:14 +0700

    update new api

--- CheckInTime/Common/NewUI/CleanWinner/WinnerViewController.swift
+++ CheckInTime/Common/NewUI/CleanWinner/WinnerViewController.swift
@@ -22,14 +22,14 @@ class WinnerViewController: UIViewController {
     @IBOutlet weak var lblNameWinner3:      UILabel!
     @IBOutlet weak var lblNameWinner4:      UILabel!
     
-    var soundDoneRandomStaff      = AVAudioPlayer()
-    var winner1         = Candicate()
-    var winner2         = Candicate()
-    var winner3         = Candicate()
-    var winner4         = Candicate()
-    let arrRandom       = [Int]() // staff index array to clean
-    let arrStaff        = [AnyObject]() // staff array to clean
-    var swipeGesture: UISwipeGestureRecognizer!
+    var soundDoneRandomStaff        = AVAudioPlayer()
+    var winner1                     = Candicate()
+    var winner2                     = Candicate()
+    var winner3                     = Candicate()
+    var winner4                     = Candicate()
+    let arrRandom                   = [Int]() // staff index array to clean
+    let arrStaff                    = [AnyObject]() // staff array to clean
+    var swipeGesture:               UISwipeGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
         swipeGesture                            = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
@@ -37,7 +37,7 @@ class WinnerViewController: UIViewController {
         imgBackground.addGestureRecognizer(swipeGesture)
         imgBackground.isUserInteractionEnabled  = true
         DispatchQueue.main.async(execute: {
-            self.setWinners(winner1: self.winner1, winner2: self.winner2, winner3: self.winner3, winner4: self.winner4)
+            self.setWinners()
         })
         let url                                 = Bundle.main.path(forResource: "bingo", ofType: "mp3")!
         do{
@@ -52,7 +52,7 @@ class WinnerViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         self.soundDoneRandomStaff.play()
     }
-    func setWinners(winner1: Candicate, winner2: Candicate, winner3: Candicate, winner4: Candicate){
+    func setWinners(){
         if winner1.name != ""{
             self.imgWinner1.image            = winner1.avatar
             self.lblNameWinner1.text         = winner1.name
@@ -75,24 +75,23 @@ class WinnerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @objc func handleSwipe(_ sender: UISwipeGestureRecognizer){
-        winner1.avatar      = nil
-        winner1.name        = nil
-        winner2.avatar      = nil
-        winner2.name        = nil
-        winner3.avatar      = nil
-        winner3.name        = nil
-        winner4.avatar      = nil
-        winner4.name        = nil
+        // remove data 
+        winner1.avatar               = nil
+        winner1.name                 = nil
+        winner2.avatar               = nil
+        winner2.name                 = nil
+        winner3.avatar               = nil
+        winner3.name                 = nil
+        winner4.avatar               = nil
+        winner4.name                 = nil
+        self.imgWinner1.image        = nil
+        self.lblNameWinner1.text     = ""
+        self.imgWinner2.image        = nil
+        self.lblNameWinner2.text     = ""
+        self.imgWinner3.image        = nil
+        self.lblNameWinner3.text     = ""
+        self.imgWinner4.image        = nil
+        self.lblNameWinner4.text     = ""
         self.dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
